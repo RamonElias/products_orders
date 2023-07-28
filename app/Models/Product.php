503e1e6e4d9fc273438de39972a718655fade0b3
Ramon Elias <eliasramondos@gmail.com>
2023-07-28 16:34:28 -0400

    migrations and relations created

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The orders that belong to the product.
+     */
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class, 'orders_lines', 'product_id', 'order_id')->withPivot('qty');
+    }
+}
